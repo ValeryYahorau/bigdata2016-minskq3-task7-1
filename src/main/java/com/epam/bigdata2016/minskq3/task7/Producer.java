07e51d6eecf28f6198f8bb6e3851f35e6b4be630
ValeryYahorau <yehorav.valery@gmail.com>
2016-09-26 15:29:38 +0300

    Producer for kafka.

--- src/main/java/com/epam/bigdata2016/minskq3/task7/Producer.java
+++ src/main/java/com/epam/bigdata2016/minskq3/task7/Producer.java
@@ -29,6 +29,7 @@ public class Producer {
                     try(Stream<String> lines = Files.lines(filePath, Charset.forName("ISO-8859-1"))) {
                         lines.forEach(line ->{
                             producer.send(new ProducerRecord<>("logstopic", line));
+                            System.out.println("Line was read from file: " + line);
                         });
                     } catch (IOException ex) {
                         throw new RuntimeException(ex);
